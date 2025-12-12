5a20c5a3c036da9cad165981b59fb60cdedbbbed
typography <rodrigo.villegaszambrano@vtsd.com>
2025-12-12 09:24:42 -0500

    in Progress

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,7 @@
 /* Put code for the task below */
+function functionName(){
+    document.getElementById('buttonTask1').addEventListener('mouseover', buttom);
+}
+function () {
+    document.getElementById("goodbye").style.visibility = "hidden";
+}
